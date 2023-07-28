ca32d230c8fa078f75525a35f41d3ac251d15ae3
indira-5 <indu109@gmail.com>
2023-07-28 16:32:57 -0700

    CollectionsExamplesDay27July

---
+++ SecondCode/DictExample.py
@@ -0,0 +1,8 @@
+d1={101:'abc',102:'def',103:'hel',104:'app'}
+print(type(d1))
+print(d1)
+d1[105]='cde'
+d1[102]='mnop'
+print(d1)
+d1.pop(103)
+print(d1)
---
+++ SecondCode/RangeExample.py
@@ -0,0 +1,5 @@
+a1 = range(1,11,1)
+print(a1)
+print(a1[0])
+print(a1[5])
+
